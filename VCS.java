ecf78b2e8982187d2eedfbbc034562ec896264fa
Matthew Obetz <mobetz@gmail.com>
2024-12-09 11:33:04 -0500

    branching notes

--- VCS.java
+++ VCS.java
@@ -95,6 +95,10 @@ public class VCS {
 
         Before we do that, we should make sure we save everything we're working on right now to a commit!
 
+        Once I checkout, I should also create a branch at that point and checkout that branch using the 'git branch <branch_name>' command.
+
+        You can also list branches just by typing 'git branch' alone.
+
 
         */
     }
