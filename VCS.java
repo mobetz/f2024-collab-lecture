1488235e14a2bca68b47c34fc9e2177bb08dea1b
Matthew Obetz <mobetz@gmail.com>
2024-12-09 11:45:13 -0500

    final notes for lecture 1

--- VCS.java
+++ VCS.java
@@ -135,7 +135,29 @@ public class VCS {
         squash their changes together.
  
 
-                  */
+        Before we do that, we should make sure we save everything we're working on right now to a commit!
+
+        Once I checkout, I should also create a branch at that point and checkout that branch using the 'git branch <branch_name>' command.
+
+        You can also list branches just by typing 'git branch' alone.
+
+        What if we wanted to get the best of both worlds, and have all the changes from both branches of the history?
+        We can do that with one final command for today: git merge. Git merging lets us take two different branches, and
+        squash their changes together.
+
+        We can do this on the command line by switching to the branch that should "receive" the changes, and then using
+        'git merge <other_branch>'.
+
+        The panes on the left and right show us both timelines, and the middle shows the final result.
+        Sometimes, you might have to make touch ups here when git can't figure out which copy of the code you want to
+        use. However, everything looks good for us, so we're ready to commit!
+
+        Git is an invaluable tool for helping us keep track of the way our code has changed over time. We can even use
+        branches to carry out different 'experiments' on our code, and only keep those that work out!
+
+        Next class, we'll see how git also has useful features for letting multiple people work together.
+          */
+
 
     }
 
